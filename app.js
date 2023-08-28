297c7265b5b69d16dc0ef119313ed3e584b8cd81
Daniyal ali <daniyalali12568@gmail.com>
2023-08-29 03:01:08 +0500

    filterReduceMap

--- app.js
+++ app.js
@@ -67,8 +67,8 @@
 
 //! Topic : String Method
 
-let firstName="daniyal";
-let lastName = "ali";
+// let firstName="daniyal";
+// let lastName = "ali";
 //check the firstName start with d or not 
 // console.log(`${firstName}`.startsWith('d'));// boolean return karayga ans correct hay to true otherwise false
 //check the lasttName start with d or not 
@@ -158,55 +158,169 @@ let lastName = "ali";
 // Asyn and await (data fetch )
 
 
-//for in loop (OBJECT)
+//for of loop (ARRAY)
+// ["", "", ""]
+// [{}, {}, {}]
 
-const programming = ["js", "rb", "py", "java", "cpp"]
+// const arr = [1, 2, 3, 4, 5]
 
-// for (const key in programming) {
-//     console.log(programming[key]);
+// for(const item of arr) {
+//     console.log(item);
+// }
+// console.log(number);
+
+// const greetings = "Hello world!"
+// for (const item of greetings) {
+//     console.log(item);
 // }
 
-programming.forEach( ( item ,index ,arr )=>{
-      console.log(item ,index , arr)
-} )
+// Maps
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// console.log(map);
+//           destrucuture||
+// for (const [key , value] of map) {
+//     console.log(`${key} :- ${value}`);
+// }
+
+//for IN loop (OBJECTS or ARRAY)
+// const myObject = {
+//     game1: 'NFS',
+//     game2: 'Spiderman'
+// }
+
+// for (const key in myObject) {
+//     // console.log(key);
+//     console.log(`${key} and value is ${myObject[key]}`);
+// }
+
+// const programming = ["js", "rb", "py", "java", "cpp"];
+// for (const key in programming) {
+//     console.log(`array key ${key} and value ${programming[key]}`);
+// }
+
+//? FOREACH LOOP
+// const coding = ["js", "ruby", "java", "python", "cpp"];
+
+// coding.forEach( (item , index , arr)=> {
+//     console.log(`arrya value ${item} and index${index} arr ${arr}`);
+// })
+
+// function printme(item){
+//     console.log(item);
+// }
+// coding.forEach(printme)
 
 // const myCoding = [
-//       {
-//           languageName: "javascript",
-//           languageFileName: "js"
-//       },
-//       {
-//           languageName: "java",
-//           languageFileName: "java"
-//       },
-//       {
-//           languageName: "python",
-//           languageFileName: "py"
-//       },
-//   ]
-//   programming.forEach(( item )=>{
-//       console.log(item.languageName);
-//   } )
+    // {
+    //     languageName: "javascript",
+    //     languageFileName: "js"
+    // },
+    // {
+    //     languageName: "java",
+    //     languageFileName: "java"
+    // },
+    // {
+    //     languageName: "python",
+    //     languageFileName: "py"
+    // },
+// ]
+// myCoding.forEach( (item)=> {
+//     console.log(item.languageName);
+// })
 
-  const myCoding = [
-      {
-          languageName: "javascript",
-          languageFileName: "js"
-      },
-      {
-          languageName: "java",
-          languageFileName: "java"
-      },
-      {
-          languageName: "python",
-          languageFileName: "py"
-      },
-  ]
-  
-  myCoding.forEach( (item ) => {
-      
-      console.log(item.languageName );
-  } )
+// forEach not return  a value
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+// const values = coding.forEach( (item) => {
+//     //console.log(item);
+//     return item
+// } )
+
+// console.log(values);
+
+
+
+
+// const values = coding.forEach( (item) => {
+//     //console.log(item);
+//     return item
+// } )
+// console.log(values);
+
+//? FILLTER value return karata hay
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const newNum = myNums.filter( (num )=>{
+//     return num > 2;
+// } )
+// console.log(newNum);
+
+// const books = [
+//     { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+//     { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+//     { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+//     { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+//     { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+//     { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+//     { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+//     { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+//     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+//   ];
+
+// const post = books.map((items)=>{
+//     return
+// })
+
+
+//   let userBook1 = books.filter( (bk) => bk.genre === 'History')
+//   console.log(userBook1);
+
+//   const userbook = books.filter( (bk) => {
+//     return bk.publish>= 1980  && bk.genre === "History" 
+//   } )
+//   console.log(userbook)
+
+//   const userbook2 = books.filter((bk)=>{
+//     return bk.edition > 2009
+//   })
+//   console.log(userbook2);
+
+//? MAP  return karta hay
+// const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNum = myNumers
+// .map((num)=> num*10 )
+// .map( (num)=> num+1  )
+// .filter( (num)=>  num >= 30)
+// console.log(newNum);
+
+// const myNumers1 = [1, 2, 3, 4, 5,5, 6, 7, 8, 9, 10,10]
+// let uniqArr = myNumers1.map((num)=> num).map((num)=> num*2)
+// console.log(uniqArr);
+
+//? REDUCE return karta hay
+const myNums = [1, 2, 3]
+
+const rarr = myNums.reduce( (acc , item)=> {
+    console.log(`${acc} and ${item}`);
+    return acc + item
+},0 )
+console.log(rarr);
+
+
+
+
+
+
+
+
+
+
 
 
 
